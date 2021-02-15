7bb70e29ebee631c82a2e4f2f57e7661ba018610
b6320500549 <jirasin.po@ku.th>
2021-02-15 19:34:36 +0700

    Update 12.c

--- 12.c
+++ 12.c
@@ -1,22 +1,22 @@
 #include<stdio.h>
 int main()
 {
-int i=0,num,k,j,time[48]={0},max=0,x;
-scanf("%d",&num);
-for(i=0;i<num;i++)
-{
-scanf("%d %d",&k,&j);
-time[k-1]+=j;
-}
-for(i=0;i<48;i++)
-{
-if(max<time[i])
-{
-max=time[i];
-x=i;
-}
-}
-printf("%d %d",x+1,time[x]);
+    int i=0,num,k,j,time[48]= {0},max=0,x;
+    scanf("%d",&num);
+    for(i=0; i<num; i++)
+    {
+        scanf("%d %d",&k,&j);
+        time[k-1]+=j;
+    }
+    for(i=0; i<48; i++)
+    {
+        if(max<time[i])
+        {
+            max=time[i];
+            x=i;
+        }
+    }
+    printf("%d %d",x+1,time[x]); //ตอนแรกเซฟไฟล์สลับกลัวคนตรวจงงไฟล์12ก็จะมีการเปลี่ยนแปลงของทั้ง12และ13
 }
 
 
