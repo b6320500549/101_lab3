45127821e5cd69e6dc93977958b94d20379759da
b6320500549 <jirasin.po@ku.th>
2021-02-15 19:03:43 +0700

    Update 14.c

--- 14.c
+++ 14.c
@@ -4,8 +4,15 @@ int main()
   long long a;
   scanf("%lld",&a);
   if(a>=0&&a<=2000000000)
-  if(a%2 == 0)
-  printf("%lld",a-1);
-  else
+  {
+  if(a==0||a==1||a==2)
+  printf("%lld",2);
+  if((a!=0&&a!=1&&a!=2)&&a%2==0)
+  {
+   printf("%lld",a-1);
+  }
+  else if(a!=0&&a!=1&&a!=2)
   printf("%lld",a);
+  }
+
 }
