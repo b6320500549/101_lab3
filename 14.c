3a44133467121662c188b74ca7f523c3bc81ff1e
b6320500549 <jirasin.po@ku.th>
2021-02-15 19:54:33 +0700

    Update 14.c

--- 14.c
+++ 14.c
@@ -1,21 +1,18 @@
 #include<stdio.h>
 int main()
 {
-	long i=0;
+	long long i=0;
 	if(i>=0&&i<=2000000000)
-		scanf("%ld",&i);
+		scanf("%lld",&i);
 	if(i==0||i==1)
 		printf("2");
 	while(i>1)
 	if((i%2==0||i%3==0||i%5==0||i%7==0)&&(i!=2 && i!=3 && i!=5 && i!=7))
 		i--;
-
 	else
     {
-		printf("%ld ",i);
+		printf("%lld ",i);
 		break;
 	}
-
-
 	return 0;
 }
